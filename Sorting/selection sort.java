ccbaa5712df46f81f4be7fed3c9364a10172ef5b
Sanjay <sanjusathish522@gmail.com>
2026-06-19 09:39:02 +0530

    Add selection sort

---
+++ Sorting/selection sort.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n-1;i++){
+            int k = i;
+            for(int j=i+1;j<n;j++){
+                if(nums[j]<nums[k]){
+                    k=j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[k];
+            nums[k] = temp;
+        }
+        return nums;
+    }
+}
